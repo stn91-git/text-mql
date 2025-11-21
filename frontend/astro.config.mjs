da715df1f56f38b711d3951147e371bca6353a3d
Divyam Verma <divyam.verma@plutomoney.in>
2025-11-21 16:46:33 +0530

    -

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,18 +3,27 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import node from '@astrojs/node';
+import sitemap from '@astrojs/sitemap';
 
+const port = 4324;
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',   // 👈 important for Docker
+    port,
+  },
+  site: 'https://textmql-dev-app.station91.in',
   integrations: [react()],
-
   vite: {
     plugins: [tailwindcss()],
-    server: {
-      allowedHosts: ['textmql-dev-app.station91.in']
+    resolve: {
+      alias: {
+        // Prevent client-side bundling of server-only packages
+      },
     },
-    preview: {
-      allowedHosts: ['textmql-dev-app.station91.in']
-    }
-  }
+  },
+  adapter: node({
+    mode: 'standalone'
+  })
 });
